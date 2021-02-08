20cc875cec0b4eff0dcd7f79a8a8f0cbce0af909
mosuke5 <w.vamos603@gmail.com>
2021-02-08 23:06:39 +0900

    add opentelemetry

---
+++ config/initializers/opentelemetory.rb
@@ -0,0 +1,14 @@
+require 'opentelemetry/sdk'
+require 'opentelemetry/exporter/jaeger'
+
+OpenTelemetry::SDK.configure do |c|
+  c.use 'OpenTelemetry::Instrumentation::Rails'
+  c.use 'OpenTelemetry::Instrumentation::Mysql2'
+  c.add_span_processor(
+    OpenTelemetry::SDK::Trace::Export::BatchSpanProcessor.new(
+      exporter: OpenTelemetry::Exporter::Jaeger::AgentExporter.new(host: 'localhost', port: 6831)
+    )
+  )
+  c.service_name = 'my-app'
+  c.service_version = '0.1.0'
+end
